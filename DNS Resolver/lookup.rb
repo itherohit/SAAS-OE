ca435f4d54526e994b107e39fe8a59b8f972bc71
itherohit <55776181+itherohit@users.noreply.github.com>
2020-01-27 22:11:54 +0530

    Formatted

--- DNS Resolver/lookup.rb
+++ DNS Resolver/lookup.rb
@@ -25,20 +25,20 @@ def parse_dns(dns_raw)
     record_type_A=[]
     record_type_A_IP=[]
     record_type_CNAME=[]
-    record_type_CNAME_destination=[]
+    record_type_CNAME_alias=[]
     #adds each line to dns array and splipt them with ","
-    dns_raw.each do |str|
-        dns.push([str.split(',')])     
+    dns_raw.each do |lines_in_files|
+        dns.push([lines_in_files.split(',')])     
     end 
     
     #Checks for recordA,IP or recordCNAME and adds them to the respected array
-    dns.each do |str|
-        if str[0][0]=="A"
-            record_type_A.push(str[0][1].strip)
-            record_type_A_IP.push(str[0][2].strip)
-        elsif str[0][0]=="CNAME"
-            record_type_CNAME.push(str[0][1].strip)
-            record_type_CNAME_destination.push(str[0][2].strip) 
+    dns.each do |words_in_files|
+        if words_in_files[0][0]=="A"
+            record_type_A.push(words_in_files[0][1].strip)
+            record_type_A_IP.push(words_in_files[0][2].strip)
+        elsif words_in_files[0][0]=="CNAME"
+            record_type_CNAME.push(words_in_files[0][1].strip)
+            record_type_CNAME_alias.push(words_in_files[0][2].strip) 
         end
     end
 
@@ -46,43 +46,41 @@ def parse_dns(dns_raw)
     record_A={
         :type => "A",
         :source => record_type_A,
-        :destination => record_type_A_IP
+        :ip => record_type_A_IP
     }
 
     #recordCNAME hash stores values of recordCNAME
-    recordCNAME={
+    record_CNAME={
         :type => "CNAME",
         :source => record_type_CNAME,
-        :destination => record_type_CNAME_destination 
-
+        :alias => record_type_CNAME_alias 
     }
 
     #dns_records gets both Hashes     
     dns_records={
         :A => record_A,
-        :CNAME => recordCNAME
+        :CNAME => record_CNAME
     }
     
     #returns record dns_record with two hashes
     return dns_records
-   
 end
 
 #this function adds domain details to lookup_chain
 def resolve(dns_records, lookup_chain, domain)
-    recordA=dns_records[:A]
-    recordCNAME=dns_records[:CNAME]
+    record_A=dns_records[:A]
+    record_CNAME=dns_records[:CNAME]
     #if domain in recordA
     #add destination to lookup_chain
-    if recordA[:source].include?(domain)
-        lookup_chain.push(recordA[:destination][recordA[:source].index(domain)])
+    if record_A[:source].include?(domain)
+        lookup_chain.push(record_A[:ip][record_A[:source].index(domain)])
     #if domain in recordCNAME
     #add destination to lookup_chain
     #update domain with destination
     #call the funtion again with new domain(Recursion)
-    elsif recordCNAME[:source].include?(domain)
-        lookup_chain.push(recordCNAME[:destination][recordCNAME[:source].index(domain)])
-        domain=recordCNAME[:destination][recordCNAME[:source].index(domain)]
+    elsif record_CNAME[:source].include?(domain)
+        lookup_chain.push(record_CNAME[:alias][record_CNAME[:source].index(domain)])
+        domain=record_CNAME[:alias][record_CNAME[:source].index(domain)]
         resolve(dns_records, lookup_chain, domain)
     else 
         return lookup_chain 
@@ -92,8 +90,9 @@ end
 dns_records=parse_dns(dns_raw)
 lookup_chain = [domain]
 lookup_chain = resolve(dns_records, lookup_chain, domain)
+
 if lookup_chain.length()!=1
     puts lookup_chain.join('=>')
 else    
-    puts "ERROR : NOT FOUND"
+    puts "ERROR : Record Not Found For #{domain}"
 end    
